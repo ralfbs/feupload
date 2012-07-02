1d890313788dacc67ff74ba90d0f17c8d4e5bca3
Ralf Schneider <ralf@hr-interactive.com>
2012-07-02 11:48:04 +0200

    added defaultVisibiltySettings: Visibily switch is shown to user if non default visibilty is selected

--- Classes/Domain/Validator/FileUploadValidator.php
+++ Classes/Domain/Validator/FileUploadValidator.php
@@ -25,7 +25,6 @@
 ***************************************************************/
 
 
-
 /**
  * File upload validator
  *
@@ -33,39 +32,43 @@
  * @subpackage Domain\Validator
  * @author Pascal Dürsteler
  */
-
-class Tx_Feupload_Domain_Validator_FileUploadValidator extends Tx_Extbase_Validation_Validator_AbstractValidator 
+class Tx_Feupload_Domain_Validator_FileUploadValidator extends Tx_Extbase_Validation_Validator_AbstractValidator
 {
-	
-	/**
-	 * @param 	mixed 		$dummy		This is empty because the property is not in $_REQUEST but in $_FILES
-	 * @return	boolean
-	 */
-	public function isValid($dummy)
-	{
-		$property = $this->options['property'];
-		
-		if( $_FILES['tx_feupload_upload']['tmp_name'][$property][$property] ) 
-		{
-			$extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['feupload']);
-			
-			// This is not really a security check, but it does what it should.
-			preg_match('/(.*)\.([^\.]*$)/', $_FILES['tx_feupload_upload']['name'][$property][$property], $matches);
-			$ext = strtolower($matches[2]);
-			
-			if( in_array($ext, explode(',', $extConf['allowedFileExtensions'])) ) return true;
-			else
-			{
-				$this->errors[] = new Tx_Extbase_Validation_Error(Tx_Extbase_Utility_Localization::translate('error.' . $property . 'WrongFileType.message', 'feupload'), 1332941667);
-				return false;
-			}
-		}
-		else
-		{
-			$this->errors[] = new Tx_Extbase_Validation_Error(Tx_Extbase_Utility_Localization::translate('error.' . $property . 'Empty.message', 'feupload'), 1332507175);
-			return false;
-		}
-	}
+
+
+
+    /**
+     *
+     * @param mixed $dummy
+     *            is empty because the property is not in $_REQUEST but in
+     *            $_FILES
+     * @return boolean
+     */
+    public function isValid($dummy)
+    {
+        $property = $this->options['property'];
+        
+        if ($_FILES['tx_feupload_upload']['tmp_name'][$property][$property]) {
+            $extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['feupload']);
+            
+            // This is not really a security check, but it does what it should.
+            preg_match('/(.*)\.([^\.]*$)/', $_FILES['tx_feupload_upload']['name'][$property][$property], $matches);
+            $ext = strtolower($matches[2]);
+            
+            if (in_array($ext, explode(',', $extConf['allowedFileExtensions']))) {
+                return true;
+            } else {
+                $this->errors[] = new Tx_Extbase_Validation_Error(
+                        Tx_Extbase_Utility_Localization::translate('error.' . $property . 'WrongFileType.message', 'feupload'), 
+                        1332941667);
+                return false;
+            }
+        } else {
+            $this->errors[] = new Tx_Extbase_Validation_Error(
+                    Tx_Extbase_Utility_Localization::translate('error.' . $property . 'Empty.message', 'feupload'), 1332507175);
+            return false;
+        }
+    }
 }
 
 ?>
